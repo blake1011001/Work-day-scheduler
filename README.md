ad53879b214a588e4b7f4a39e491f8c6467c96b6
Blake Sanders <blakesand00@gmail.com>
2023-03-02 22:21:19 -0500

    fixed html and added to css, js, and README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Work-day-scheduler
 
-# Description 
+## Description 
+This allows you to plan and structure your TODO list accordingly while showing you what task is up next.
+
+## Tasks Completed 
+* When you open the planner it'll tell you the current date and time.
+* When you scroll down you're presented with timeblocks for the business hours.
+* Color coded for past, present, and future.
+* Can click the save button to store the data in local storage so when you refresh you can continue to see data.
+
+### Links 
+* https://github.com/blake1011001/Work-day-scheduler
+* https://blake1011001.github.io/Work-day-scheduler/
+## Installation
+![127 0 0 1_5500_index html](https://user-images.githubusercontent.com/121069049/222623005-af6c8698-d25d-4554-927d-d303b99b6555.png)
+### Usage 
+Used to plan out your work day. Works on any browser
+
+### Credits 
+Refractored by Blake S
+* W3schools.com
+* Youtube.com
--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
+// Tells the date and time and added an event listener 
+
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM D YYYY, H:MM:SS A")); 
+
+  $(".saveBtn").on("click", function () {
+      console.log(this);
+      var text = $(this).siblings(".description").val(); 
+      var time = $(this).parent().attr("id"); 
+      localStorage.setItem(time, text);
+  })
+  
+  // The local storage of the hours 
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  // Used link to get current time and date 
+
+  function hourTracker() {
+      var currentTimeHour = moment().hour(); 
+
+      // Used "if, else" loop to add or remove given the time
+      $(".time-block").each(function () {
+          var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+          console.log( blockHour, currentTimeHour)
+          if (blockHour < currentTimeHour) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }
+          else if (blockHour === currentTimeHour) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+          }
+      })
+  }
+  hourTracker(); 
+})
   
